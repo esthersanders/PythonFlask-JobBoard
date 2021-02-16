589579e5349da33360a23776f054d66306ab63ac
estherviolin <esthernaomi.violin@gmail.com>
2021-02-16 12:27:03 -0600

    module 3

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+PATH = sqlite3.connect('db/jobs.sqlite')
+
+open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+        connection, g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+
+    return connection
+execute_sql(sql, values =(), commit = False, single = False):
+    connection=open_connection()
+    connection.exec(sql, values)
+    if commit === True:
+        results = connection.commit()
+    else:
+        results = if cursor.fetchone() if single else cursor.fetchall()
+    return results
+
+@app.teardown_appcontext
+close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        close_connection()
 
 app = Flask(__name__)
 
